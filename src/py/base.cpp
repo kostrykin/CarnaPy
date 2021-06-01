1ccfc520dc334474d67e487795cde57820726ee4
Leonid Kostrykin <kostrykin@evoid.de>
2021-06-01 18:38:28 +0200

    migrates from boost::python to pybind11

--- src/py/base.cpp
+++ src/py/base.cpp
@@ -1,40 +1,40 @@
-#include <boost/python.hpp>
+#include <pybind11/pybind11.h>
+#include <pybind11/eigen.h>
+
+namespace py = pybind11;
+
+using namespace pybind11::literals; // enables the _a literal
+
 #include <Carna/base/FrameRenderer.h>
 #include <Carna/base/Node.h>
 
-using namespace boost::python;
 using namespace Carna::base;
 
-// See also:
-// https://www.boost.org/doc/libs/1_63_0/libs/python/doc/html/tutorial/index.html
-// https://wiki.python.org/moin/boost.python/HowTo
-// https://stackoverflow.com/questions/24568501/exposing-c-functions-that-return-pointer-using-boost-python
-// https://doc.nektar.info/developerguide/5.0.2/developer-guidese55.html
-// https://www.boost.org/doc/libs/1_58_0/libs/python/doc/v2/class.html
-// https://www.boost.org/doc/libs/1_60_0/libs/python/doc/html/faq/how_can_i_wrap_a_function_which0.html
-// https://stackoverflow.com/questions/16731115/how-to-debug-a-python-segmentation-fault
-
-void Node__attachChild( Node& self, std::auto_ptr< Spatial > child )
+template< typename T >
+void _py_free( T* ptr )
 {
-    return self.attachChild( child.release() );
+    delete ptr;
 }
 
-BOOST_PYTHON_MODULE(base)
+// see: https://pybind11.readthedocs.io/en/stable/advanced/misc.html#generating-documentation-using-sphinx
+
+PYBIND11_MODULE(base, m)
 {
-    //class_<FrameRenderer>("FrameRenderer", init<, unsigned int, unsigned int>())
-    //    .def("width", &FrameRenderer::width)
-    //    .def("height", &FrameRenderer::height);
 
-    class_< Spatial, std::auto_ptr< Spatial >, boost::noncopyable >( "Spatial", init< optional< const std::string& > >() )
-        .def( "has_parent", &Spatial::hasParent )
-        .def( "detach_from_parent", &Spatial::detachFromParent, return_value_policy< manage_new_object >() );
-        //.def( "tag", &Spatial::tag, return_value_policy< return_by_value >() );
-        //.def( "tag", &Spatial::tag, return_value_policy< copy_const_reference >() );
-        //.add_property( "tag", make_function( &Spatial::tag, return_value_policy< copy_const_reference >() ), &Spatial::setTag );
+    m.def( "free", &_py_free< Spatial > );
 
-    class_< Node, bases< Spatial >, std::auto_ptr< Node >, boost::noncopyable >( "Node", init< optional< const std::string& > >() )
-        .def( "attach_child", &Node__attachChild )
-        .def( "detach_child", &Node::detachChild, return_value_policy< manage_new_object >() )
+    py::class_< Spatial >( m, "Spatial" )
+        .def( "detach_from_parent", &Spatial::detachFromParent, py::return_value_policy::reference )
+        .def_property_readonly( "has_parent", &Spatial::hasParent )
+        .def_property_readonly( "parent", py::overload_cast<>( &Spatial::parent, py::const_ ) )
+        .def_property( "movable", &Spatial::isMovable, &Spatial::setMovable )
+        .def_property( "tag", &Spatial::tag, &Spatial::setTag )
+        .def_readwrite( "local_transform", &Spatial::localTransform );
+
+    py::class_< Node, Spatial >( m, "Node" )
+        .def_static( "create", []( const std::string& tag ) { return new Node( tag ); }, py::return_value_policy::reference, "tag"_a = "" )
+        .def( "attach_child", &Node::attachChild )
+        .def( "detach_child", &Node::detachChild, py::return_value_policy::reference )
         .def( "has_child", &Node::hasChild )
         .def( "delete_all_children", &Node::deleteAllChildren )
         .def( "children", &Node::children );
--- test/test.py
+++ test/test.py
@@ -1,14 +1,25 @@
-import sys
-from IPython.core import ultratb
-sys.excepthook = ultratb.FormattedTB(mode='Verbose', color_scheme='Linux', call_pdb=False)
-
 import base
+import numpy as np
 
-node1 = base.Node()
+import faulthandler
+faulthandler.enable()
+
+node1 = base.Node.create()
 assert node1.children() == 0
-node2 = base.Node()
+node2 = base.Node.create()
 node1.attach_child(node2)
 assert node1.children() == 1
 
-#node1.delete_all_children()
-#print(node2.tag())
+base.free(node1)
+
+node1 = base.Node.create("root")
+assert node1.tag == "root"
+node2 = base.Node.create()
+assert not node2.has_parent
+node1.attach_child(node2)
+assert node2.has_parent
+assert node2.parent is node1
+node2 = node2.detach_from_parent()
+assert np.allclose(node2.local_transform, np.eye(4))
+base.free(node1)
+base.free(node2)
