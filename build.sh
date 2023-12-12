0e64533ab2793f9917d44aaba3e28383f562c9a0
Leonid Kostrykin <void@evoid.de>
2023-12-12 17:48:20 +0100

    Fix build script

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 # Install dependencies:
 sudo apt-get install -y libegl1-mesa-dev libboost-iostreams-dev
@@ -10,7 +11,7 @@ tar -vzxf eigen-3.2.10.tar.gz -C /tmp/
 export CMAKE_PREFIX_PATH="/tmp/eigen-3.2.10:$CMAKE_PREFIX_PATH"
 
 # Build and install Carna
-git clone git@github.com:kostrykin/Carna.git build_carna
+git clone https://github.com/kostrykin/Carna.git build_carna
 cd build_carna
 sh linux_build.sh
 
