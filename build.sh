58e342b72df16648088d30d5aca93a8acbdd98d9
Leonid Kostrykin <void@evoid.de>
2023-12-12 21:35:26 +0100

    Fix build script

--- build.sh
+++ build.sh
@@ -5,17 +5,21 @@ set -e
 sudo apt-get install -y libegl1-mesa-dev libglu1-mesa-dev libglew-dev libboost-iostreams-dev
 conda install -y -c conda-forge pybind11
 
+# Setup and check dependencies:
+export PYBIND11_PREFIX="$CONDA_PREFIX/share/cmake/pybind11"
+cat "$PYBIND11_PREFIX/pybind11Config.cmake" >/dev/null
+
 # Get Eigen sources:
 wget https://gitlab.com/libeigen/eigen/-/archive/3.2.10/eigen-3.2.10.tar.gz
 tar -zxf eigen-3.2.10.tar.gz -C /tmp/
 export CMAKE_PREFIX_PATH="/tmp/eigen-3.2.10:$CMAKE_PREFIX_PATH"
 
-# Build and install Carna
+# Build and install Carna:
 git clone https://github.com/kostrykin/Carna.git build_carna
 cd build_carna
 sh linux_build-egl.sh
 
-# Build and install CarnaPy
+# Build and install CarnaPy:
 cd ..
 python setup.py build
 python setup.py install
